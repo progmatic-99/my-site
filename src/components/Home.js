28aab269d06d46e9dd02a6bf648e2e09a154a83c
Shubham Upreti <shubham.upreti@gmail.com>
2021-08-26 10:34:08 +0530

    components add

---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "@fortawesome/fontawesome-free-solid";
+import "@fortawesome/free-brands-svg-icons";
+import "@styles/App.scss";
+
+export class Home extends Component {
+  render() {
+    return (
+      <main id="home">
+        <h1 className="lg-heading">
+          Shubham <span className="text-secondary">Upreti</span>
+        </h1>
+        <h2 className="sm-heading">Developer | Engineer | Freelancer</h2>
+        <div className="icons">
+          <a
+            href="https://twitter.com/progmatic99"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "twitter"]} size="2x" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/shubham-upreti/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" />
+          </a>
+          <a
+            href="https://github.com/progmatic-99"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "github"]} size="2x" />
+          </a>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Home;
